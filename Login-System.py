12eb3588bb7458227ed1dafca8c19eefacb4d277
Mitchell <Mitchellseng02@gmail.com>
2023-05-03 14:01:19 +0100

    Added login page!

--- Login-System.py
+++ Login-System.py
@@ -1,6 +1,13 @@
-def Login():
+def main():
     while True:
-        if (username == "" & password == ""):
-            continue
+        username = input("Enter the admin username: ")
+        password = input("Enter the admin password: ")
+        if (username == "admin" and password == "Admin1!"):
+            break
         else:
             print ("Incorrect Username or Password!")
+            continue
+    
+    print ("You entered the correct credentials")
+
+main()
